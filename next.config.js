4f059fdb7833cd2f54561af3a4cb388a908ec87f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-02 19:09:11 +0300

    next.js config for nextImage

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ['giphy.com', 'media3.giphy.com', '*'],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    iconSizes: [],
+    path: '/_next/image',
+    loader: 'default',
+  },
+}
